65d7bf08dca9200f65bf4b1614bacc59994d5543
Angel142330 <angel142330@gmail.com>
2025-10-15 12:57:16 +0200

    Add initial project structure with Jenkins pipeline, application code, and unit tests

---
+++ mi-proyecto/src/main/java/com/miempresa/App.java
@@ -0,0 +1,17 @@
+package com.miempresa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Maven!" );
+    }
+
+    public static int suma(int a, int b) {
+        return a + b;
+    }
+}
---
+++ mi-proyecto/src/test/java/com/miempresa/AppTest.java
@@ -0,0 +1,16 @@
+package com.miempresa;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    
+{
+    @Test
+    public void testSuma() {
+        assertEquals(5, App.suma(2, 3));
+    }
+}
